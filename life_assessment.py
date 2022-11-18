ca771a707745db96d3ea406b002541190b512d6d
Edgar Cure <edgardavid1503@gmail.com>
2022-11-18 15:52:12 -0800

    Updated files

---
+++ life_assessment.py
@@ -0,0 +1,33 @@
+import csv
+from datetime import datetime
+import math
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+def add_data():
+    """This function will ask for data about work, school, life and marriage, 
+    and will store it in a csv file called '#_data.csv' 
+    """
+    pass
+
+def read_data():
+    """This function will read the data and save it in a local list 
+    to be used in other functions
+    """
+    pass
+
+def assess_data():
+    """This function will use the data for further assesment"""
+    pass
+
+
+
+
--- receipt.py
+++ receipt.py
@@ -1,31 +1,100 @@
 import csv
+from datetime import datetime
 
 requested_product_number_index = 0
 requested_product_quantity_index = 1
+store_name = "Inkom Emporium"
 
 
 def main():
-    product_number_index = 0
-    products_dict = read_dict("products.csv", product_number_index )
-    print(products_dict)
-    print()
-    print("Requested Items")
-    
-    with open("request.csv", "rt") as request_file:
-        reader = csv.reader(request_file)
-        next(reader)
+    try:
+        product_number_index = 0
+        products_dict = read_dict("products.csv", product_number_index )
+        print()
+        print(store_name)
+        print()
+        print("Items")
+        total_quantity_requested = 0
+        
+        with open("request.csv", "rt") as request_file:
+            reader = csv.reader(request_file)
+            next(reader)
+            subtotal = 0
+           
 
-        for row in reader:
-            product_requested = row[requested_product_number_index]
-            quantity_requested = int(row[requested_product_quantity_index])
-            
-            if product_requested in products_dict:
+            for row in reader:
+                
+                product_requested = row[requested_product_number_index]
+                quantity_requested = int(row[requested_product_quantity_index])
+                
+                
+                
+                
                 value = products_dict[product_requested]
                 product_name = value[1]
                 product_price = float(value[2])
                 print(f"{product_name}: {quantity_requested} @ {product_price}")
+                total_quantity_requested += quantity_requested
+                
+                subtotal_per_item = quantity_requested * product_price
+                subtotal +=subtotal_per_item
+                
+                
+
+            
+
+            
+
+            date = datetime.now()
+            weekday = date.weekday()
+            if weekday == 1 or weekday == 2:
+                print()
+                print(f"Number of items:", total_quantity_requested)
+                discount = subtotal * 0.1
+                subtotal = subtotal - discount
+                sales_tax = subtotal * 0.06
+                total = subtotal + sales_tax
+                print("Congratulations! It is Thursday and you get a 10% discount")
+                print(f"Your subtotal with discount is:  {subtotal:.2f}")
+                print(f"Sales tax: {sales_tax:.2f}")
+                print(f"Total: {total:.2f}")
+                
+            else:
+                print()
+                print(f"Number of items:", total_quantity_requested)
+                sales_tax = subtotal * 0.06
+                total = subtotal + sales_tax
+                print(f"Sales tax: {sales_tax:.2f}")
+                print(f"Total: {total:.2f}")
+                
+             
+            print()
+            
+        
+            print()
+            print("Thanks for shopping at the Inkom Emporium.")
+            
+            print(f"{date:%a %b %d %I:%M %p %Y }")
+            print()
+            
+            
+    
 
                 
+    except FileNotFoundError as fil_err:
+        print(fil_err)
+        print("Choose a different file")
+        
+    except PermissionError as permission_err:
+        print(permission_err)
+        print("Choose a different file")
+
+    except KeyError as key_err:
+        print(f"Error: unknown product ID in the request.csv file")
+        print(key_err)
+    
+
+  
 
 
 def read_dict(filename, key_column_index):
